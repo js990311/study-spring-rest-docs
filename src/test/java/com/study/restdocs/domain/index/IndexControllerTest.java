23d1632671850d9e8911e170b28319c86cea04d0
Lee jong sik <102013392+js990311@users.noreply.github.com>
2024-12-20 20:16:35 +0900

    config: restdocs

---
+++ src/test/java/com/study/restdocs/domain/index/IndexControllerTest.java
@@ -0,0 +1,20 @@
+package com.study.restdocs.domain.index;
+
+import com.study.restdocs.config.AbstractRestDocsTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class IndexControllerTest extends AbstractRestDocsTest {
+
+    @Test
+    void indexes() throws Exception {
+        mockMvc.perform(
+                get("/")
+        ).andExpect(status().isOk());
+    }
+}
