dc525a3525281d9ef5058e79d194fc73ca13ac5a
Lee jong sik <102013392+js990311@users.noreply.github.com>
2024-12-21 10:29:27 +0900

    test: add test-data

--- src/test/java/com/study/restdocs/RestdocsApplicationTests.java
+++ src/test/java/com/study/restdocs/RestdocsApplicationTests.java
@@ -2,7 +2,11 @@ package com.study.restdocs;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
 
+@Sql("/data.sql")
+@ActiveProfiles("test")
 @SpringBootTest
 class RestdocsApplicationTests {
 
--- src/test/java/com/study/restdocs/domain/index/IndexControllerTest.java
+++ src/test/java/com/study/restdocs/domain/index/IndexControllerTest.java
@@ -3,11 +3,15 @@ package com.study.restdocs.domain.index;
 import com.study.restdocs.config.AbstractRestDocsTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.junit.jupiter.api.Assertions.*;
 
+@ActiveProfiles("test")
+@Sql("/data.sql")
 @SpringBootTest
 class IndexControllerTest extends AbstractRestDocsTest {
 
