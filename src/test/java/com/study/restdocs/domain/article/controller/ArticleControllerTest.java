2b7453b00846cbdfd3a316d8f2e008c509e69520
Lee jong sik <102013392+js990311@users.noreply.github.com>
2024-12-21 18:04:14 +0900

    docs: put-article

--- src/test/java/com/study/restdocs/domain/article/controller/ArticleControllerTest.java
+++ src/test/java/com/study/restdocs/domain/article/controller/ArticleControllerTest.java
@@ -127,7 +127,26 @@ class ArticleControllerTest extends AbstractRestDocsTest {
                         .contentType("application/json")
                         .content(objectMapper.writeValueAsString(map))
         ).andExpect(status().isOk())
-                .andDo(restDocs.document());
+                .andDo(restDocs.document())
+                .andDo(
+                        MockMvcRestDocumentationWrapper.document(
+                                "put-article",
+                                ResourceDocumentation.resource(
+                                        ResourceSnippetParameters.builder()
+                                                .tag("articles")
+                                                .summary("article 새로 작성함")
+                                                .requestFields(
+                                                        fieldWithPath("id").description("수정할 article의 id"),
+                                                        fieldWithPath("content").description("작성할 article의 본문")
+                                                )
+                                                .responseFields(
+                                                        ArticleDtoDocs.docs("")
+                                                )
+                                                .build()
+                                )
+                        )
+                )
+        ;
     }
 
     static class ArticleDtoDocs{
